c790d076323a51acf8b0c2980fd73feb4a18ef3e
Swati Kushwaha <swatikushwaha3110@gmail.com>
2025-12-26 13:23:51 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(() => ({
-  base: "/",   // ✅ VERCEL FIX
+export default defineConfig({
+  base: "/",          // ✅ Vercel
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: "index.js",
-        chunkFileNames: "chunk-[name].js",
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
-            return "index.css";
-          }
-          return "asset-[name][extname]";
-        },
-      },
-    },
-  },
-}));
+});
